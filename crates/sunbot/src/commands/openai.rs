54a3ccaf0dff3560c0281bb9a98b14e1232b8101
Aaron Foley <aaron.s.foley@gmail.com>
2024-11-29 22:09:23 +1100

    switch to uploading image attachments for genimage

--- crates/sunbot/src/commands/openai.rs
+++ crates/sunbot/src/commands/openai.rs
@@ -1,9 +1,15 @@
+use std::vec;
+
 use crate::{Context, Error};
 use async_openai::types::{
     ChatCompletionRequestSystemMessageArgs, CreateChatCompletionRequestArgs,
-    CreateImageRequestArgs, Image, ImageModel, ImageSize,
+    CreateImageRequestArgs, Image, ImageModel, ImageResponseFormat, ImageSize,
 };
-use serenity::builder::CreateEmbed;
+use base64::prelude::*;
+use serenity::{
+    all::{CreateAttachment, EditMessage},
+    builder::CreateEmbed,
+};
 
 /// Ask a question to OpenAI
 #[poise::command(slash_command)]
@@ -52,15 +58,9 @@ pub async fn genimage(
     ctx: Context<'_>,
     #[description = "The prompt to send to OpenAI"] prompt: String,
     #[description = "The size of the image to generate"] size: Option<ImageSizeType>,
+    #[description = "The number of images to generate"] amount: Option<u8>,
 ) -> Result<(), Error> {
     let client = ctx.data().openai_client.as_ref().unwrap();
-
-    let embed = CreateEmbed::new()
-        .title("Please wait generating images")
-        .field("Prompt", prompt.as_str(), false)
-        .field("Requstor", ctx.author().tag(), false);
-
-    let reply = ctx.send(poise::CreateReply::default().embed(embed)).await?;
 
     let image_size = match size.unwrap_or(ImageSizeType::Large) {
         ImageSizeType::Small => ImageSize::S256x256,
@@ -74,30 +74,72 @@ pub async fn genimage(
         _ => ImageModel::Other(ctx.data().config.openai.genimage.model.clone()),
     };
 
+    let mut embed = CreateEmbed::new()
+        .title("Please wait generating images")
+        .field("Prompt", prompt.as_str(), false)
+        .field("Requstor", format!("<@!{}>", ctx.author().id), false);
+
+    let n = if model == ImageModel::DallE3 {
+        if amount.unwrap_or(1) > 1 {
+            embed = embed.description("NOTE: Only 1 image can be generated with DALL-E 3");
+        }
+        1
+    } else {
+        amount.unwrap_or(1)
+    };
+
+    let reply = ctx.send(poise::CreateReply::default().embed(embed)).await?;
+
     let request = CreateImageRequestArgs::default()
         .model(model)
+        .n(n)
         .prompt(prompt.as_str())
         .size(image_size)
+        .response_format(ImageResponseFormat::B64Json)
         .build()
         .unwrap();
 
     let resp = client.images().create(request).await.unwrap();
 
-    let mut builder = poise::CreateReply::default();
+    let mut builder = EditMessage::new();
 
-    let embeds = resp
-        .data
-        .iter()
-        .map(|image| match image.as_ref() {
+    let mut embeds = vec![CreateEmbed::new()
+        // Setting this so that the embeds are joined together
+        .url("https://openai.com")
+        .field("Prompt", prompt.as_str(), false)
+        .field("Requstor", format!("<@!{}>", ctx.author().id), false)];
+
+    for (pos, image) in resp.data.iter().enumerate() {
+        match image.as_ref() {
             Image::Url {
                 url,
                 revised_prompt: _,
-            } => CreateEmbed::new().image(url),
-            _ => CreateEmbed::new().description("Unknown image type"),
-        })
-        .collect();
+            } => {
+                embeds.push(CreateEmbed::new().url("https://openai.com").image(url));
+            }
+            Image::B64Json {
+                b64_json,
+                revised_prompt: _,
+            } => {
+                let filename = format!("image-{}.png", pos);
+                let data = BASE64_STANDARD.decode(b64_json.as_bytes()).unwrap();
+                builder = builder.new_attachment(CreateAttachment::bytes(data, &filename));
+                embeds.push(
+                    CreateEmbed::new()
+                        .url("https://openai.com")
+                        .attachment(filename),
+                );
+            }
+        }
+    }
 
-    builder.embeds = embeds;
-    reply.edit(ctx, builder).await?;
+    builder = builder.embeds(embeds);
+    // Calling this via the message object as the reply does not send new attachments
+    reply
+        .into_message()
+        .await
+        .unwrap()
+        .edit(ctx, builder)
+        .await?;
     Ok(())
 }
